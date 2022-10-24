0dcdcc4f96c4b00ccd8df0abc83e5f1b0596a9c9
iankerich254 <iankerich254@gmail.com>
2022-10-24 19:34:36 +0300

    Correction of 0-lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-contains lookup function
-"""
+"""contains lookup function"""
+
 def lookup(obj):
     """returns a list of available attributes and methods of an object"""
     return dir(obj)
