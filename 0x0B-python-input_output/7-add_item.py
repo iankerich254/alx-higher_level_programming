e9c023a4c0767d52ff8d05d9009bb0c512b320f5
iankerich254 <iankerich254@gmail.com>
2022-10-25 22:14:11 +0300

    Function that adds all arguments to a Python list, and saves them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-script that adds all arguments to a Python list, and then saves them to a file
+script that adds all arguments to a Python list, and saves them to a file
 """
 
 from sys import argv
