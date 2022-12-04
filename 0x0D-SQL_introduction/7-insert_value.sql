b6b60e5fef8fae7dd1edc420eac0be13f2b038f1
Ian Kerich <iankerich254@gmail.com>
2022-12-04 11:42:12 +0300

    First add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,5 @@
--- inserts a new row in the table first_table (database hbtn_0c_0) 
---	in your MySQL server
+-- inserts a new row in the table first_table (database hbtn_0c_0)
+-- in your MySQL server
 -- New row:
 --     id = 89
 --     name = Holberton School
