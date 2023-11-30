a946ae645b8283b8ee0b6e171bb5251af33c3515
Ian Kerich <iankerich254@gmail.com>
2023-11-30 12:51:06 -0800

    Make correction to 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_arg_ke(argv):
+def print_arg(argv):
     n = len(argv) - 1
     if n == 0:
         print("{:d} argument.".format(n))
@@ -14,4 +14,4 @@ def print_arg_ke(argv):
 
 if __name__ == "__main__":
     import sys
-    print_arg_ke(sys.argv)
+    print_arg(sys.argv)
