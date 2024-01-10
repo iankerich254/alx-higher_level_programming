269ec0ee5d5c7824ad15a7fbbe2aa54c7ca000c2
Ian Kerich <iankerich254@gmail.com>
2024-01-10 04:39:06 +0300

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+contains the 'class_to_json' function
+"""
+
+
+def class_to_json(obj):
+    """returns dictionary description with simple data structure for JSON
+    serialization of an object"""
+    return obj.__dict__
