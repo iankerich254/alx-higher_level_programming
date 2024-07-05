53560097f491b4ceddb0444a51c0f5298b732ea6
Ian Kerich <iankerich254@gmail.com>
2024-07-06 00:15:38 +0300

    Task 0: What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status & displays info about the response.
+"""
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
